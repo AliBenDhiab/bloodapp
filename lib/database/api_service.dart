ba80aaf1433a1f1b1bc2d7394a3f3148e44222c8
Ali Ben Dhiab <aloohfoka@gmail.com>
2023-02-26 11:42:51 +0300

    first commit

---
+++ lib/database/api_service.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: avoid_print
+
+import 'package:http/http.dart' as http;
+import 'contsen.dart';
+
+import 'city_model.dart';
+
+class ApiService {
+  Future<List<CityModel>>? getcity() async {
+    try {
+      var url = Uri.parse(ApiConstants().url + ApiConstants().enpoint);
+      var response = await http.get(url, headers: headers);
+      if (response.statusCode == 200) {
+        List<CityModel> model = userModelFromJson(response.body)!;
+        return model;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return [];
+  }
+
+  Map<String, String> headers = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Charset': 'utf-8'
+  };
+}
